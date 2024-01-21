6652f473d0b1769509eeb0fb3098ba0daa75b5ac
unknown <ShoaibAttanKhan@gmail.com>
2024-01-21 22:00:46 +0530

    JWT_SECRET 3.2

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -2,6 +2,7 @@ import NextAuth from 'next-auth'
 import GoogleProvider from 'next-auth/providers/google';
 
 export const authOptions = {
+    secret: process.env.JWT_SECRET,
     providers: [
         GoogleProvider({
             clientId: process.env.GOOGLE_CLIENT_ID,
@@ -9,7 +10,6 @@ export const authOptions = {
 
         })
     ],
-    secret: process.env.JWT_SECRET,
     pages: {
         signIn: '/auth/signin',
     },
